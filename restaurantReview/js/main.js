4a42bc42426f94275e71960d051362bbb8caf011
chunlei2 <chunleiliu1995@gmail.com>
2019-06-23 09:36:04 -0500

    modify tags after reviewed

--- restaurantReview/js/main.js
+++ restaurantReview/js/main.js
@@ -161,10 +161,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `${restaurant.name} Restaurant`;
+  image.alt = `An image of Restaurant ${restaurant.name} `;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   name.setAttribute('tabindex', '0');
   li.append(name);
--- restaurantReview/js/restaurant_info.js
+++ restaurantReview/js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `${restaurant.name} Restaurant`;
+  image.alt = `An image of Restaurant ${restaurant.name}`;
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
